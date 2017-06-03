a7fe218bf2cfd3a0f454929a40a11bf651c00dee
Md Jawed Akhtar <zahid7292@gmail.com>
2017-06-03 11:39:54 +0530

    automation enabled

--- ekanban-web-test/src/main/java/com/example/ekanban/Automation.java
+++ ekanban-web-test/src/main/java/com/example/ekanban/Automation.java
@@ -1,14 +1,60 @@
 package com.example.ekanban;
 
-import com.example.ekanban.util.AutomationUtil;
-import com.example.ekanban.util.Constants;
+import com.example.ekanban.util.PropertiesUtil;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import java.io.File;
 
 /**
  * Created by mtalam on 5/21/2017.
  */
-public class Automation {
+public class Automation extends BaseClass {
+    private final Logger logger = LoggerFactory.getLogger(Automation.class);
 
-    public static void main(String[] args){
-        System.out.println(AutomationUtil.getProperty(Constants.USER_NAME));
+    @Test
+    public void testLogin() {
+        String baseUrl = PropertiesUtil.getProperty("base.url");
+        String email = PropertiesUtil.getProperty("user.email");
+        String password = PropertiesUtil.getProperty("user.password");
+
+        driver.get(baseUrl);
+
+        try {
+            Thread.sleep(10000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        //*[@id="content"]/div/div[2]/div[1]/div/div/div/div/form/div/div[1]/span/input
+        driver.findElement(By.xpath("//span/input[@name='email']")).sendKeys(email);
+        driver.findElement(By.xpath("//span/input[@name='password']")).sendKeys(password);
+        driver.findElement(By.xpath("//form/footer/button")).click();
+
+//        try {
+//            driver.manage().wait(2000);
+//        } catch (InterruptedException e) {
+//            e.printStackTrace();
+//        }
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        String userName = null;
+        if (!driver.findElement(By.xpath("//header/nav/a[@href='/profile']")).equals(null)) {
+            userName = driver.findElement(By.xpath("//header/nav/a[@href='/profile']")).getText();
+        }
+        Assert.assertEquals(userName, PropertiesUtil.getProperty("user.name"));
     }
+
+
+
 }
---
+++ ekanban-web-test/src/main/java/com/example/ekanban/admin/CategoryTest.java
@@ -0,0 +1,7 @@
+package com.example.ekanban.admin;
+
+/**
+ * Created by Md Jawed Akhtar on 03-06-2017.
+ */
+public class CategoryTest {
+}
